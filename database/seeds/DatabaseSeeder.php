a4e39ed7c988e3031b8b7ba4f7ac0804f5ea43c2
Jose Henrique Felipetto <jose.felipetto@gmail.com>
2020-08-30 18:47:13 -0300

    Added basic database structure for items, model, controllers, seeders and factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UserSeeder::class);
          $this->call(ProjectsSeeder::class);
+         $this->call(StatusesSeeder::class);
+         $this->call(ItemsSeeder::class);
     }
 }
